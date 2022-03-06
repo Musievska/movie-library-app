9a1be18766517e173c7eef105488bc7e43cf7ddd
Dilyana Musievska <dilyana.musievska@gmail.com>
2022-03-06 18:41:34 +0200

    login updated

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,13 +1,29 @@
 import React from "react"
 import { Link } from "react-router-dom"
-
+import { useFormik } from "formik"
+import * as Yup from "yup"
 const LoginForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema: Yup.object({
+            email: Yup.string()
+                .email("Invalid email address")
+                .required("Please enter your email address"),
+            password: Yup.string().required("Please enter your password"),
+        }),
+        onSubmit: async values => {
+            console.log(values)
+        },
+    })
     return (
         <div className="login-form-container">
             <div className="col-10 col-sm-8 col-md-5 mx-auto">
                 <h1 className="font-weight-bold">Login</h1>
             </div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <div className="form-group col-10 col-sm-8 col-md-5 mx-auto mt-5">
                     <label htmlFor="email">Email Address</label>
                     <input
@@ -15,7 +31,13 @@ const LoginForm = () => {
                         id="email"
                         name="email"
                         type="email"
+                        {...formik.getFieldProps("email")}
                     />
+                    {formik.touched.email && formik.errors.email ? (
+                        <small className="form-text text-danger">
+                            {formik.errors.email}
+                        </small>
+                    ) : null}
                 </div>
                 <div className="form-group col-10 col-sm-8 col-md-5 mx-auto">
                     <label htmlFor="password">Password</label>
@@ -24,7 +46,13 @@ const LoginForm = () => {
                         id="password"
                         name="password"
                         type="password"
+                        {...formik.getFieldProps("password")}
                     />
+                    {formik.touched.password && formik.errors.password ? (
+                        <small className="form-text text-danger">
+                            {formik.errors.password}
+                        </small>
+                    ) : null}
                 </div>
                 <div className="col-10 col-sm-8 col-md-5 mx-auto">
                     <button type="submit" className="btn btn-lg btn-primary btn-block">
